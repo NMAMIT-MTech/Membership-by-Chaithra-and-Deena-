e5b57afb70f8fb75655b8f1490d727d52da8633b
NMAMIT-MTech <37292759+NMAMIT-MTech@users.noreply.github.com>
2018-05-28 19:46:43 +0530

    Create BusinessLogic.java

---
+++ BusinessLogic.java
@@ -0,0 +1,51 @@
+
+package BL;
+
+import Beans.Member;
+import Beans.MemberDetails;
+import DAO.MemberDAO;
+import DAO.MemberDetailsDAO;
+import java.sql.SQLException;
+import java.util.List;
+
+public class BusinessLogic {
+    
+public boolean insertMemberData(Member objMember) throws SQLException{
+		
+		MemberDAO objMem=new MemberDAO();
+
+		boolean success=objMem.insertintoMember(objMember);
+                return success;
+	}
+
+
+public boolean insertMemberDetailsData(MemberDetails objMember) throws SQLException{
+		
+		Date d=objMember.getStartDate();
+		objMember.setExpiryDate(new Date(d.getDate(),d.getMonth(),d.getYear()+1));
+		
+		MemberDetailsDAO objMemDet=new MemberDetailsDAO();
+
+		boolean success=objMemDet.insertintoMemberDetails(objMember);
+                return success;
+	}
+
+
+public List<MemberDetails> getMemberDetailsData(String MemIDs) throws SQLException{
+		
+		MemberDetailsDAO objMemDet=new MemberDetailsDAO();
+
+		List<MemberDetails> lstDet=objMemDet.getMemberDetails(MemIDs);
+
+		return lstDet;
+	}
+
+	public List<Member> getMemberData(String MemIDs) throws SQLException{
+		
+		MemberDAO objMem=new MemberDAO();
+
+		List<Member> lstDet=objMem.getMemberDetails(MemIDs);
+
+		return lstDet;
+	}
+}
