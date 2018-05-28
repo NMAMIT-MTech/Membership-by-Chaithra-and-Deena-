21b8a842ad629fb22e686fc5cbfdfc3a652c160b
NMAMIT-MTech <37292759+NMAMIT-MTech@users.noreply.github.com>
2018-05-28 19:48:36 +0530

    Create MemberDAO.java

---
+++ MemberDAO.java
@@ -0,0 +1,77 @@
+package DAO;
+
+import BL.Constants;
+import Beans.Member;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MemberDAO {
+    public boolean insertintoMember(Member objMem) throws SQLException{
+
+boolean success=false;
+Connection con=null;
+Statement stm=null;
+try{
+Class.forName(Constants.DriverName);
+
+con=DriverManager.getConnection(Constants.URL,Constants.UserName,Constants.Pass);
+
+stm= con.createStatement();
+
+String sql="Insert into Member values("+objMem.getName()+","+objMem.getMemberType()+","+objMem.getContactNumber()+","+objMem.getEmail()+","+objMem.getAddress()+","+objMem.getAadhar()+")";
+
+success=stm.execute(sql);
+}catch(Exception e){
+	e.printStackTrace();
+}finally{
+	stm.close();
+	con.close();
+}
+
+return success;    
+}
+
+public List<Member> getMemberDetails(String MemIDs) throws SQLException{
+Connection con=null;
+Statement stm=null;
+List<Member> lstMem= new ArrayList<Member>();
+try{
+Class.forName(Constants.DriverName);
+
+con=DriverManager.getConnection(Constants.URL,Constants.UserName,Constants.Pass);
+
+stm= con.createStatement();
+
+String sql="Select * from Member where MemberID in("+MemIDs+")";
+
+ResultSet rs=stm.executeQuery(sql);
+
+while(rs.next()){
+	Member objMem= new Member();
+	objMem.setContactNumber(rs.getLong("ContactNumber"));
+        objMem.setMemberType(rs.getString("memberType"));
+	objMem.setName(rs.getString("Name"));
+	objMem.setEmail(rs.getString("Email"));
+	objMem.setAadhar(rs.getLong("Aadhar"));
+	objMem.setMemberID(rs.getInt("MemberID"));
+lstMem.add(objMem);
+}
+}catch(Exception e){
+	e.printStackTrace();
+}finally{
+	stm.close();
+	con.close();
+}
+
+return lstMem;
+}
+}
+
+
+
+
