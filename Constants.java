a307fcc821926fd74e75798f23f1331fd244d05c
NMAMIT-MTech <37292759+NMAMIT-MTech@users.noreply.github.com>
2018-05-28 19:47:32 +0530

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,8 @@
+package BL;
+
+public class Constants {
+  public  final static String URL="";
+public final static String UserName="";
+public final static String Pass="";
+public final static String DriverName="";
+}
