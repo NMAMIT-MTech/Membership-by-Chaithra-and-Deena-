973ada902e0c9a6c767030fd8fc793a643fe7c25
NMAMIT-MTech <37292759+NMAMIT-MTech@users.noreply.github.com>
2018-05-29 20:47:08 +0530

    Update BusinessLogic.java

--- BusinessLogic.java
+++ BusinessLogic.java
@@ -1,17 +1,17 @@
-
 package BL;
-
 import Beans.Member;
-import Beans.MemberDetails;
 import DAO.MemberDAO;
-import DAO.MemberDetailsDAO;
+import java.sql.Date;
 import java.sql.SQLException;
 import java.util.List;
-
 public class BusinessLogic {
     
-public boolean insertMemberData(Member objMember) throws SQLException{
+	public boolean insertMemberData(Member objMember) throws SQLException{
 		
+		//calculating expiry date
+		Date d=objMember.getStartDate();
+		objMember.setExpiryDate(new Date(d.getDate(),d.getMonth(),d.getYear()+1));
+               
 		MemberDAO objMem=new MemberDAO();
 
 		boolean success=objMem.insertintoMember(objMember);
@@ -19,32 +19,11 @@ public boolean insertMemberData(Member objMember) throws SQLException{
 	}
 
 
-public boolean insertMemberDetailsData(MemberDetails objMember) throws SQLException{
-		// calculating expiry date after getting startDate
-		Date d=objMember.getStartDate();
-		objMember.setExpiryDate(new Date(d.getDate(),d.getMonth(),d.getYear()+1));
-		
-		MemberDetailsDAO objMemDet=new MemberDetailsDAO();
-
-		boolean success=objMemDet.insertintoMemberDetails(objMember);
-                return success;
-	}
-
-
-public List<MemberDetails> getMemberDetailsData(String MemIDs) throws SQLException{
-		
-		MemberDetailsDAO objMemDet=new MemberDetailsDAO();
-
-		List<MemberDetails> lstDet=objMemDet.getMemberDetails(MemIDs);
-
-		return lstDet;
-	}
-
-	public List<Member> getMemberData(String MemIDs) throws SQLException{
+	public List<Member> getMemberData() throws SQLException{
 		
 		MemberDAO objMem=new MemberDAO();
 
-		List<Member> lstDet=objMem.getMemberDetails(MemIDs);
+		List<Member> lstDet=objMem.getMemberDetails();
 
 		return lstDet;
 	}
